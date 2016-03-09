a156f07f8d74d63464ae02e30699f470639ba700
Josh Pollock <jpollock412@gmail.com>
2016-03-08 22:53:59 -0500

    Add Freemius

--- ingot-edd.php
+++ ingot-edd.php
@@ -23,6 +23,43 @@ add_action( 'ingot_before', function(){
 	if( ! defined( 'INGOT_EDD_VER' ) ) {
 		define( 'INGOT_EDD_VER', '1.0.0' );
 		require_once dirname( __FILE__ ) . '/vendor/autoload.php';
+
+		/**
+		 * Freemius setup
+		 *
+		 * @since 1.0.0
+		 *
+		 * @return \Freemius
+		 */
+		function ingot_edd_fs() {
+			global $ingot_edd_fs;
+
+			if ( ! isset( $ingot_edd_fs ) ) {
+				ingot_fs();
+				$ingot_edd_fs = fs_dynamic_init( array(
+					'id'                => '223',
+					'slug'              => 'edd',
+					'public_key'        => 'pk_432d5762f0117a9590536ef4739e9',
+					'is_premium'        => false,
+					'has_paid_plans'    => false,
+					'is_org_compliant'  => false,
+					'parent'      => array(
+						'id'         => '210',
+						'slug'       => 'ingot',
+						'public_key' => 'pk_e6a19a3508bdb9bdc91a7182c8e0c',
+						'name'       => 'Ingot',
+					),
+				) );
+			}
+
+			return $ingot_edd_fs;
+		}
+
+		/**
+		 * Boot Freemius integration
+		 */
+		add_action( 'ingot_loaded', 'ingot_edd_fs', 25 );
+
 		new add_destinations();
 		new tracking();
 	}
